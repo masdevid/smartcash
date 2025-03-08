370fd468da1f3c386e2151eef36fee0f277c6e9a
Devid HW <devid.wahid@gmail.com>
2025-03-08 16:27:29 +0700

    fix fpn_pan init

--- models/necks/__init__.py
+++ models/necks/__init__.py
@@ -4,6 +4,5 @@ Author: Alfrida Sabar
 Deskripsi: Package initialization untuk neck models.
 """
 
-from smartcash.models.necks.fpn_pan import FeatureProcessingNeck, PathAggregationNeck   
-
-__all__ = ['FeatureProcessingNeck', 'PathAggregationNeck']
+from smartcash.models.necks.fpn_pan import FeatureProcessingNeck, PathAggregationNetwork, FeaturePyramidNetwork
+__all__ = ['FeatureProcessingNeck', 'PathAggregationNetwork', 'FeaturePyramidNetwork']

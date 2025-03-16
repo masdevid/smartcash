e658e730a6be4edc94fd030cf7cd6a3c9db2aff8
Devid HW <devid.wahid@gmail.com>
2025-03-17 05:39:39 +0700

    copy

---
+++ copy.sh
@@ -0,0 +1,7 @@
+cp -r smartcash/model .
+cp -r smartcash/config .
+cp -r smartcash/components .
+cp -r smartcash/ui .
+cp -r smartcash/dataset .
+cp -r smartcash/detection .
+cp -r smartcash/common .

eb89e2836fc11e1809f8432e05f3ebf85fda9b22
Devid HW <devid.wahid@gmail.com>
2025-03-18 06:14:16 +0700

    update

--- model/architectures/backbones/__init__.py
+++ model/architectures/backbones/__init__.py
@@ -4,6 +4,7 @@ Deskripsi: Inisialisasi modul backbones dengan ekspor komponen yang diperlukan
 """
 
 from smartcash.model.architectures.backbones.base import BaseBackbone
+from smartcash.model.architectures.backbones.cspdarknet import CSPDarknet
 from smartcash.model.architectures.backbones.efficientnet import (
     EfficientNetBackbone, 
     SUPPORTED_MODELS, 

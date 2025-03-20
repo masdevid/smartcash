fae91f25e7ba09108e4d1565547124999781d491
Devid HW <devid.wahid@gmail.com>
2025-03-21 03:00:50 +0700

    update

--- components/observer/event_topics_observer.py
+++ components/observer/event_topics_observer.py
@@ -57,6 +57,7 @@ class EventTopics:
     PREPROCESSING_START = "preprocessing.start"
     PREPROCESSING_PROGRESS = "preprocessing.progress"
     PREPROCESSING_CURRENT_PROGRESS = "preprocessing.current_progress"
+    PREPROCESSING_STEP_PROGRESS = "preprocessing.step_progress"
     PREPROCESSING_END = "preprocessing.end"
     PREPROCESSING_ERROR = "preprocessing.error"
     

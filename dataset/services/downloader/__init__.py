6e5cdcbdce3b3efa7f9797dff676d48df7980c98
Devid HW <devid.wahid@gmail.com>
2025-03-25 12:03:27 +0700

    update

--- dataset/services/downloader/__init__.py
+++ dataset/services/downloader/__init__.py
@@ -6,13 +6,13 @@ Deskripsi: Ekspor komponen layanan download dataset untuk digunakan di luar pake
 from smartcash.dataset.services.downloader.download_service import DownloadService
 from smartcash.dataset.services.downloader.download_validator import DownloadValidator
 from smartcash.dataset.services.downloader.roboflow_downloader import RoboflowDownloader
-from smartcash.dataset.services.downloader.file_processor import FileProcessor
+from smartcash.dataset.services.downloader.file_processor import DownloadFileProcessor
 from smartcash.dataset.services.downloader.backup_service import BackupService
 
 __all__ = [
     'DownloadService',
     'DownloadValidator',
     'RoboflowDownloader',
-    'FileProcessor',
+    'DownloadFileProcessor',
     'BackupService',
 ]

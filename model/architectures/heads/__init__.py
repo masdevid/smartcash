717a3d21e7fbdf382270400547c99dd9a26aba52
Devid HW <devid.wahid@gmail.com>
2025-05-05 12:21:31 +0700

    fix: prep for prod

--- model/architectures/heads/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-File: smartcash/model/architectures/heads/__init__.py
-Deskripsi: File init untuk mengekspor komponen detection head
-"""
-
-from smartcash.model.architectures.heads.detection_head import DetectionHead, LAYER_CONFIG
-
-__all__ = ['DetectionHead', 'LAYER_CONFIG']

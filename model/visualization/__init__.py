668bf5b8b12eeecfed1b009cb3ca6023936b2ff4
Devid HW <devid.wahid@gmail.com>
2025-05-15 04:19:28 +0700

    update

--- model/visualization/__init__.py
+++ model/visualization/__init__.py
@@ -8,6 +8,7 @@ Deskripsi: Modul untuk file __init__.py
 from smartcash.model.visualization.metrics_visualizer import MetricsVisualizer, plot_confusion_matrix
 from smartcash.model.visualization.evaluation_visualizer import EvaluationVisualizer
 from smartcash.model.visualization.detection_visualizer import DetectionVisualizer
+from smartcash.model.visualization.setup_visualization import setup_visualization
 
 
 from smartcash.model.visualization.research.research_visualizer import ResearchVisualizer
@@ -22,5 +23,6 @@ __all__ = [
     'ExperimentVisualizer',
     'ScenarioVisualizer',
     'EvaluationVisualizer',
-    'DetectionVisualizer'
+    'DetectionVisualizer',
+    'setup_visualization'
 ]

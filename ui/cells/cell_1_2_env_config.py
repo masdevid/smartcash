e3fb94725150dae9b5d816bcc910d477a85b6214
Devid HW <devid.wahid@gmail.com>
2025-05-20 00:19:28 +0700

    update

--- ui/cells/cell_1_2_env_config.py
+++ ui/cells/cell_1_2_env_config.py
@@ -5,8 +5,13 @@ Deskripsi: Entry point untuk environment configuration cell
 
 def setup_env_config():
     """Setup dan tampilkan UI untuk konfigurasi environment."""
-    from smartcash.ui.setup.env_config import initialize_env_config_ui
-    return initialize_env_config_ui()
+    from smartcash.ui.setup.env_config.components import EnvConfigComponent
+    
+    # Create and display the environment configuration component
+    env_config = EnvConfigComponent()
+    env_config.display()
+    
+    return env_config
 
 # Eksekusi saat modul diimpor
 ui_components = setup_env_config()
--- ui/setup/env_config/components/__init__.py
+++ ui/setup/env_config/components/__init__.py
@@ -3,6 +3,6 @@ File: smartcash/ui/setup/env_config/components/__init__.py
 Deskripsi: Package untuk komponen UI konfigurasi environment
 """
 
-from smartcash.ui.setup.env_config.components.env_config_component import create_env_config_ui
+from smartcash.ui.setup.env_config.components.env_config_component import EnvConfigComponent
 
-__all__ = ['create_env_config_ui']
+__all__ = ['EnvConfigComponent']

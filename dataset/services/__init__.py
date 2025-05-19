a2bac9484920e77f73c03d8906bc988168dad973
Devid HW <devid.wahid@gmail.com>
2025-05-20 05:37:43 +0700

    update

--- dataset/services/__init__.py
+++ dataset/services/__init__.py
@@ -24,9 +24,12 @@ from smartcash.dataset.services.preprocessor.cleaner import PreprocessedCleaner
 
 # Augmentor services
 from smartcash.dataset.services.augmentor.augmentation_service import AugmentationService
+from smartcash.dataset.services.augmentor.dataset_augmentor import DatasetAugmentor
 from smartcash.dataset.services.augmentor.image_augmentor import ImageAugmentor
 from smartcash.dataset.services.augmentor.bbox_augmentor import BBoxAugmentor
 from smartcash.dataset.services.augmentor.pipeline_factory import AugmentationPipelineFactory
+from smartcash.dataset.services.augmentor.augmentation_worker import AugmentationWorker
+from smartcash.dataset.services.augmentor.class_balancer import ClassBalancer
 
 # Downloader services
 from smartcash.dataset.services.downloader.download_service import DownloadService
@@ -73,9 +76,12 @@ __all__ = [
     
     # Augmentor
     'AugmentationService',
+    'DatasetAugmentor',
     'ImageAugmentor',
     'BBoxAugmentor',
     'AugmentationPipelineFactory',
+    'AugmentationWorker',
+    'ClassBalancer',
     
     # Downloader
     'DownloadService',
--- dataset/services/augmentor/__init__.py
+++ dataset/services/augmentor/__init__.py
@@ -1,11 +1,22 @@
 """
 File: smartcash/dataset/services/augmentor/__init__.py
-Deskripsi: Package initialization untuk augmentor service
+Deskripsi: Package untuk layanan augmentasi dataset
 """
 
-from smartcash.dataset.services.augmentor.pipeline_factory import AugmentationPipelineFactory
-from smartcash.dataset.services.augmentor.bbox_augmentor import BBoxAugmentor       
+from smartcash.dataset.services.augmentor.dataset_augmentor import DatasetAugmentor
 from smartcash.dataset.services.augmentor.image_augmentor import ImageAugmentor
+from smartcash.dataset.services.augmentor.bbox_augmentor import BBoxAugmentor
+from smartcash.dataset.services.augmentor.pipeline_factory import AugmentationPipelineFactory
+from smartcash.dataset.services.augmentor.augmentation_worker import AugmentationWorker
+from smartcash.dataset.services.augmentor.class_balancer import ClassBalancer
 from smartcash.dataset.services.augmentor.augmentation_service import AugmentationService
 
-__all__ = ['AugmentationPipelineFactory', 'BBoxAugmentor', 'ImageAugmentor', 'AugmentationService']
+__all__ = [
+    'DatasetAugmentor',
+    'ImageAugmentor',
+    'BBoxAugmentor',
+    'AugmentationPipelineFactory',
+    'AugmentationWorker',
+    'ClassBalancer',
+    'AugmentationService'
+]

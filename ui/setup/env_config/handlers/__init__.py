7d3eae5a2fc68c09c877272c34257defd08be4f2
Devid HW <devid.wahid@gmail.com>
2025-05-20 00:29:24 +0700

    update

--- ui/setup/env_config/handlers/__init__.py
+++ ui/setup/env_config/handlers/__init__.py
@@ -1,8 +1,9 @@
 """
 File: smartcash/ui/setup/env_config/handlers/__init__.py
-Deskripsi: Package untuk handler UI konfigurasi environment
+Deskripsi: Module untuk handlers konfigurasi environment
 """
 
-from smartcash.ui.setup.env_config.handlers.setup_handlers import setup_env_config_handlers
+from smartcash.ui.setup.env_config.handlers.setup_handlers import EnvConfigHandlers
+from smartcash.ui.setup.env_config.handlers.auto_check_handler import AutoCheckHandler
 
-__all__ = ['setup_env_config_handlers']
+__all__ = ['EnvConfigHandlers', 'AutoCheckHandler']

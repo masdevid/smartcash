ff2fc66a05a8ed22b964af9c2632ac8bdaf9e85b
Devid HW <devid.wahid@gmail.com>
2025-05-23 02:26:06 +0700

    update

--- ui/setup/env_config/utils/__init__.py
+++ ui/setup/env_config/utils/__init__.py
@@ -2,10 +2,3 @@
 File: smartcash/ui/setup/env_config/utils/__init__.py
 Deskripsi: Package untuk utilitas environment config
 """
-
-from smartcash.ui.setup.env_config.utils.config_utils import init_config_manager, ensure_config_dir
-
-__all__ = [
-    'init_config_manager',
-    'ensure_config_dir'
-]

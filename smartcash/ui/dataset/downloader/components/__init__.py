f437fc6a56ee75c5d239b3711c3f2befc6855be8
Devid HW <devid.wahid@gmail.com>
2025-06-05 04:07:05 +0700

    update

--- smartcash/ui/dataset/downloader/components/__init__.py
+++ smartcash/ui/dataset/downloader/components/__init__.py
@@ -2,8 +2,3 @@
 File: smartcash/ui/dataset/downloader/components/__init__.py
 Deskripsi: Components entry point dengan reusable UI factories
 """
-
-from .ui_layout import create_downloader_ui
-from .ui_form import create_form_fields
-
-__all__ = ['create_downloader_ui', 'create_form_fields']
--- smartcash/ui/dataset/downloader/handlers/__init__.py
+++ smartcash/ui/dataset/downloader/handlers/__init__.py
@@ -2,19 +2,3 @@
 File: smartcash/ui/dataset/downloader/handlers/__init__.py
 Deskripsi: Handlers entry point dengan clean imports dan factories
 """
-
-from .validation_handler import validate_download_parameters
-from .defaults import DEFAULT_CONFIG, VALIDATION_RULES, get_default_api_key
-
-__all__ = [
-    # Config management
-    'DEFAULT_CONFIG',
-    
-    # Handler setup
-        
-    # Validation
-    'validate_download_parameters', 'VALIDATION_RULES',
-    
-    # Utils
-    'get_default_api_key'
-]

e9e1c51d951a8b8e3f2a0ed286bf6977f29fbad5
Devid HW <devid.wahid@gmail.com>
2025-06-05 10:56:52 +0700

    update

--- ui/dataset/downloader/__init__.py
+++ ui/dataset/downloader/__init__.py
@@ -3,11 +3,7 @@ File: smartcash/ui/dataset/downloader/__init__.py
 Deskripsi: Simple entry point yang langsung return UI widget
 """
 
-from .downloader_init import initialize_downloader_ui, get_downloader_status, setup_downloader
+from smartcash.ui.dataset.downloader.downloader_init import initialize_downloader_ui, get_downloader_status
 
-__all__ = ['initialize_downloader_ui', 'get_downloader_status', 'setup_downloader']
+__all__ = ['initialize_downloader_ui', 'get_downloader_status']
 
-# Main public API
-def setup_downloader(env=None, config=None, **kwargs):
-    """Main entry point - return UI widget langsung."""
-    return initialize_downloader_ui(env=env, config=config, **kwargs)

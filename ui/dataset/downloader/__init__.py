c60019aa0b025a8a62e75cb8056798024127abed
Devid HW <devid.wahid@gmail.com>
2025-06-07 04:10:50 +0700

    update

--- ui/dataset/downloader/__init__.py
+++ ui/dataset/downloader/__init__.py
@@ -3,7 +3,7 @@ File: smartcash/ui/dataset/downloader/__init__.py
 Deskripsi: Simple entry point yang langsung return UI widget
 """
 
-from smartcash.ui.dataset.downloader.downloader_init import initialize_downloader_ui
+from smartcash.ui.dataset.downloader.downloader_init import initialize_downloader
 
-__all__ = ['initialize_downloader_ui']
+__all__ = ['initialize_downloader']
 

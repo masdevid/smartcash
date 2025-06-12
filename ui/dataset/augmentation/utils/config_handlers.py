66b527b03d2a4629a93a700ff75b2b148aed3245
Devid HW <devid.wahid@gmail.com>
2025-06-12 12:51:51 +0700

    update

--- ui/dataset/augmentation/utils/config_handlers.py
+++ ui/dataset/augmentation/utils/config_handlers.py
@@ -12,9 +12,9 @@ def handle_save_config(ui_components: Dict[str, Any]):
     
     # Kirimkan dictionary ui_components yang berisi semua widget
     clear_ui_outputs({
-        'log_output': ui_components.log_output,
-        'status': ui_components.status,
-        'confirmation_area': ui_components.confirmation_area
+        'log_output': ui_components['log_output'],
+        'status': ui_components['status'],
+        'confirmation_area': ui_components['confirmation_area']
     })
     clear_confirmation_area(ui_components)
     
@@ -34,9 +34,9 @@ def handle_reset_config(ui_components: Dict[str, Any]):
     
     # Kirimkan dictionary ui_components yang berisi semua widget
     clear_ui_outputs({
-        'log_output': ui_components.log_output,
-        'status': ui_components.status,
-        'confirmation_area': ui_components.confirmation_area
+        'log_output': ui_components['log_output'],
+        'status': ui_components['status'],
+        'confirmation_area': ui_components['confirmation_area']
     })
     clear_confirmation_area(ui_components)
     

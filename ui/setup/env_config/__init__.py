079187f77dfc080fa40ae3cc612d6ad1a0b6f321
Devid HW <devid.wahid@gmail.com>
2025-06-24 19:21:04 +0700

    chore(env-config): clean up __init__.py exports

--- ui/setup/env_config/__init__.py
+++ ui/setup/env_config/__init__.py
@@ -5,10 +5,8 @@ Deskripsi: Entry point untuk environment configuration module SmartCash
 
 # Import only main exports untuk clean API
 from smartcash.ui.setup.env_config.env_config_initializer import initialize_environment_config_ui
-from smartcash.ui.setup.env_config.components.env_config_component import create_env_config_component
 
 # Main exports
 __all__ = [
     'initialize_environment_config_ui',
-    'create_env_config_component'
 ]

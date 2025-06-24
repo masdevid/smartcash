b5f87121ecb172152e62868f0dd3a95d53a63d12
Devid HW <devid.wahid@gmail.com>
2025-06-24 13:30:41 +0700

    Add package __init__.py files for better module organization

--- ui/__init__.py
+++ ui/__init__.py
@@ -2,14 +2,3 @@
 File: smartcash/ui/__init__.py
 Deskripsi: UI package exports
 """
-
-# Export setup module
-from .setup import (
-    initialize_env_config_ui,
-    initialize_environment_config_ui
-)
-
-__all__ = [
-    'initialize_env_config_ui',
-    'initialize_environment_config_ui'
-]

8fe1719a47dec42bf76f84609cc94c18cd1ea04d
Devid HW <devid.wahid@gmail.com>
2025-06-24 20:37:24 +0700

    cleanup

--- ui/setup/env_config/__init__.py
+++ ui/setup/env_config/__init__.py
@@ -4,9 +4,9 @@ Deskripsi: Entry point untuk environment configuration module SmartCash
 """
 
 # Import only main exports untuk clean API
-from smartcash.ui.setup.env_config.env_config_initializer import initialize_environment_config_ui
+from smartcash.ui.setup.env_config.env_config_initializer import initialize_env_config_ui
 
 # Main exports
 __all__ = [
-    'initialize_environment_config_ui',
+    'initialize_env_config_ui',
 ]

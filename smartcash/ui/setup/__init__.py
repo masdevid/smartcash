635bc77660bc062beefdbc157ba4404051372f96
Devid HW <devid.wahid@gmail.com>
2025-06-27 20:32:59 +0700

    Fix: Update import statement in setup/__init__.py

--- smartcash/ui/setup/__init__.py
+++ smartcash/ui/setup/__init__.py
@@ -4,6 +4,6 @@ Deskripsi: Setup module exports
 """
 
 from smartcash.ui.setup.env_config.env_config_initializer import initialize_env_config_ui
-from smartcash.ui.setup.dependency.dependency_initializer import initialize_dependency_ui
+from smartcash.ui.setup.dependency import initialize_dependency_ui
 
 __all__ = ['initialize_env_config_ui', 'initialize_dependency_ui']
